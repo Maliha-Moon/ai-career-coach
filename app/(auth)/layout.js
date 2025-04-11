069fdedb38c175674eba0b235d57b284446ff987
Maliha Rahman Moon <malihamoon05@gmail.com>
2025-04-11 22:49:35 +0600

    Render sign-in/sign-up

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -1,1 +1,9 @@
+import React from "react";
+
+const AuthLayuot = ({ children }) => {
+  // sign-in, sign-up render here
+  return <div className="flex justify-center pt-40">{children}</div>;
+};
+
+export default AuthLayuot;
 
