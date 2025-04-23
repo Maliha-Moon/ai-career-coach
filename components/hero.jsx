44cc8eb4e7546497b3da3353738e8c0dd30547c3
Maliha Rahman Moon <malihamoon05@gmail.com>
2025-04-23 23:50:52 +0600

    feat(ui): create career coach hero section with responsive layout

---
+++ components/hero.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <section className="w-full pt-36 md:pt-48 pb-10">
+      <div>
+        <div>
+          <h1>
+            Your AI Career Coach
+            <br />
+            Smarter Job Moves, Faster Success!
+          </h1>
+          <p>
+            Advance your career with personalized guidance, interview prep, and
+            AI-powered tools for job success.
+          </p>
+        </div>
+
+        {/* display buttons */}
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
